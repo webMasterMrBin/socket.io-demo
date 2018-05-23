61ea29f565dcd90a5bfebc18399cede99be5d874
webMasterMrBin <songlibin@caicloud.io>
2018-05-23 15:13:55 +0800

    feat: 搭建项目开发环境(项目技术栈)

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 # socket.io-demo
 socket.io 聊天室demo
-<<<<<<< HEAD
 熟悉socket.io  api 和ws的逻辑 的简单聊天室demo
-=======
->>>>>>> Initial commit
+
+2018.5.23(练手项目 功能暂定)
+1.用户登录功能
+2.node文件服务器
+3.网络聊天室
---
+++ webpack.config.js
@@ -0,0 +1,57 @@
+const webpack = require('webpack');
+const path = require('path');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+const extractLESS = new ExtractTextPlugin('css/[name].css');
+
+module.exports = {
+  mode: 'development',
+  entry: {
+		main: './public/src/js/index.js',
+  },
+  devServer: {
+    contentBase: path.join(__dirname, "public/build"),
+    hot: true,
+    proxy: {
+      '*': 'http://localhost:4000'
+    }
+  },
+  output: {
+    filename: '[name].js',
+    path: path.join(__dirname, '/public/build'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+      },
+      {
+        test: /\.less$/,
+        use: extractLESS.extract({
+            use: [{
+                  loader: "css-loader" // translates CSS into CommonJS
+                  }, {
+                  loader: "less-loader" // compiles Less to CSS
+                }],
+            fallback: "style-loader"
+          })
+      }
+    ],
+  },
+	resolve: {
+		modules: [
+			path.join(__dirname, './public/src/js'),
+			'node_modules',
+		]
+	},
+  plugins: [
+		new webpack.ProvidePlugin({
+			React: 'react',
+			ReactDOM: 'react-dom',
+		}),
+		new webpack.NamedModulesPlugin(),
+		new webpack.HotModuleReplacementPlugin(),
+		extractLESS
+  ]
+};
