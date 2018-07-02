dbe3795c4cd229c44f50a657252e2c48fe718d33
webMasterMrBin <songlibin@caicloud.io>
2018-07-02 15:27:02 +0800

    feat: 二次确认弹窗组件

--- public/src/js/component/public/windowOpen.js
+++ public/src/js/component/public/windowOpen.js
@@ -2,11 +2,16 @@ import PropTypes from "prop-types";
 import { Close } from "./icon";
 import { connect } from "react-redux";
 import classNames from "classnames";
+import { Button } from "antd";
 
-// 接口报错提示的弹窗组件
+// 接口报错提示的弹窗组件和弹窗确认的组件
 class WindowOpen extends React.Component {
   state = {
     hide: false
+  };
+
+  static defaultProps = {
+    confirm: false // confirm为true  则是弹窗确认组件
   };
 
   closeWindow = () => {
@@ -23,12 +28,34 @@ class WindowOpen extends React.Component {
   };
 
   render() {
-    const { error } = this.props;
+    const { error, confirm, confirmMsg, confirmFunc } = this.props;
 
     const modalClass = classNames({
       modal: true,
       "animate-hidden": this.state.hide
     });
+
+    if (confirm) {
+      return (
+        <div className="u-window">
+          <div className="cover" />
+          <div className={modalClass}>
+            <div onClick={this.closeWindow} className="btn-close">
+              <svg className="icon icon-close" viewBox="0 0 32 32">
+                <Close />
+              </svg>
+            </div>
+            <div className="confirm-msg">
+              {confirmMsg}
+            </div>
+            <div className="confirm-box">
+              <Button type="dashed" className="u-button" onClick={confirmFunc}>确认</Button>
+              <Button type="primary" onClick={this.closeWindow}>取消</Button>
+            </div>
+          </div>
+        </div>
+      );
+    }
 
     return (
       <div className="u-window">
@@ -47,7 +74,10 @@ class WindowOpen extends React.Component {
 }
 
 WindowOpen.propTypes = {
-  error: PropTypes.string // 父组件的错误信息
+  error: PropTypes.string, // 父组件的错误信息
+  confirm: PropTypes.bool,
+  confirmMsg: PropTypes.string, // 提示信息
+  confirmFunc: PropTypes.func
 };
 
 module.exports = connect(
--- public/src/js/reducer/home.js
+++ public/src/js/reducer/home.js
@@ -2,6 +2,9 @@
 const initialState = {
   windowOpen: false, // 是否打开错误提示的弹框
   errMsg: "", // api接口报错的信息
+  confirm: false, // 弹窗确认windowOpen
+  confirmMsg: "", // 提示信息
+  confirmFunc: () => {}, // 点击确定执行的函数
   messageOpen: false, // 请求成功提示组件
   doneMsg: "" // 请求结束的提示信息
 };
@@ -11,7 +14,10 @@ export default (state = initialState, action) => {
     case "WINDOW_OPEN":
       return Object.assign({}, state, {
         windowOpen: action.windowOpen,
-        errMsg: action.errMsg
+        errMsg: action.errMsg || "",
+        confirm: action.confirm || false,
+        confirmMsg: action.confirmMsg || "",
+        confirmFunc: action.confirmFunc
       });
     case "WINDOW_CLOSE":
       return Object.assign({}, state, {
