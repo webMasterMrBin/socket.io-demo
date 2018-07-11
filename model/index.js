aa19e046217456177ecdd41a4f3d372fd416ec49
webMasterMrBin <songlibin@caicloud.io>
2018-07-11 20:23:51 +0800

    express cant' set header when seted in client

--- model/index.js
+++ model/index.js
@@ -5,13 +5,23 @@ const file = require("./file");
 // mongose promise改用node
 mongoose.Promise = Promise;
 
-mongoose.connect("mongodb://localhost/userLogin");
+mongoose.connect("mongodb://localhost/userLogin", {
+  server: {
+    poolSize: 6, // 默认为5
+    reconnectTries: 5, // 无限重连的节奏
+    auto_reconnect: true,
+  }
+});
 
 const db = mongoose.connection;
 
 db.on("error", () => {
   console.log("mongodb error");
 });
+
+db.on("disconnected", function() {
+  console.log("mongoose disconnected");
+})
 
 db.once("open", function() {
   // we're connected!
