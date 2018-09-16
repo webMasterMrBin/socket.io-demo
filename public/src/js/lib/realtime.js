f316673e09a9b59e7a94475171e25b2f09a5d73e
webMasterMrBin <songlibin@caicloud.io>
2018-09-16 11:20:51 +0800

    生产环境socket URL跳转

--- public/src/js/lib/realtime.js
+++ public/src/js/lib/realtime.js
@@ -1,5 +1,9 @@
 import io from "socket.io-client";
 
-const socket = io.connect("http://localhost:4000");
+const socket = io.connect(
+  redux_logger !== "production"
+    ? "http://localhost:4000"
+    : "http://http://52.15.253.243:4000/"
+);
 
 export { socket };
